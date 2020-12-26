26628c9909cccba5a7bc0cb82ef8b66ee8038273
zqqqqz2000 <zqqqqz2000@sina.cn>
2020-12-27 01:27:09 +0800

    add project struct

--- hyperion_types/poc.py
+++ hyperion_types/poc.py
@@ -1,4 +1,5 @@
 from typing import *
+from core import global_var
 from hyperion_types.target import Target
 from hyperion_types.poc_tree_filter import PocTreeFilter
 
@@ -11,9 +12,16 @@ class POC(PocTreeFilter):
     def __init__(self, target: Target):
         self.g = self.execute(target)
         self.target: Target = target
+        self.logger = global_var.logger
 
     def execute(self, target: Target):
+        """
+        poc main function, should be a generator
+        """
         raise
 
     def error_handler(self, error: Exception):
+        """
+        when the poc execute with an error, the error information will send to this function and handle
+        """
         ...
