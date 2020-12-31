e41643f376a307fab3f7964d6e571aff2dab67d1
zqqqqz2000 <zqqqqz2000@sina.cn>
2020-12-31 10:58:37 +0800

    fix bug

--- logger/echo.py
+++ logger/echo.py
@@ -1,7 +1,11 @@
+import colorama
+
 from core.core_types import Logger
 from colorama import init, Fore, Back, Style
 from typing import *
 import time
+
+colorama.init(True)
 
 
 class Echo(Logger):
@@ -9,22 +13,18 @@ class Echo(Logger):
     def error(src: Any, information: str) -> NoReturn:
         t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         print(Fore.RED + f'[{t} {src}] {information}')
-        print(Style.RESET_ALL)
 
     @staticmethod
     def warning(src: Any, information: str) -> NoReturn:
         t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         print(Fore.YELLOW + f'[{t} {src}] {information}')
-        print(Style.RESET_ALL)
 
     @staticmethod
     def information(src: Any, information: str) -> NoReturn:
         t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         print(Fore.GREEN + f'[{t} {src}] {information}')
-        print(Style.RESET_ALL)
 
     @staticmethod
     def debug(src: Any, information: str) -> NoReturn:
         t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         print(Fore.BLUE + f'[{t} {src}] /DEBUG/ {information}')
-        print(Style.RESET_ALL)
